046ae3016c649233041c1db46e87ac293919f5a8
Rachel Polachova <polachova@d-tt.nl>
2022-11-02 19:26:35 +0100

    init project

---
+++ Chat/ChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatApp.swift
+//  Chat
+//
+//  Created by Rachel Polachova on 02/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
